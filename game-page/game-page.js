20fa8ec03fabe9e47339aa9e462cf127b211f057
sformichella <sformichella@gmail.com>
2020-10-13 07:53:35 -0700

    next button resets page

--- game-page/game-page.js
+++ game-page/game-page.js
@@ -7,14 +7,27 @@ import {
     default as pokemonArray
 } from '../pokemon.js';
 
+// STATE
+
+
 
 const userChoices = [...document.getElementsByName('pokemon')];
-const randomIndices = getDistinctRandomNumbers(pokemonArray.length, 3);
+let randomIndices = getDistinctRandomNumbers(pokemonArray.length, 3);
+
+const nextButton = document.getElementById('next-trio-button');
 
 for (const radio of userChoices) {
     radio.addEventListener('change', (e) => {
+
         e.target.parentElement.style.backgroundColor = 'black';
 
+        for (const radio2 of userChoices) {
+            radio2.disabled = true;
+            radio2.nextElementSibling.style.opacity = 0.5;
+        }
+
+        nextButton.classList.toggle('hidden');
+        
     })
 
     const radioIndex = userChoices.indexOf(radio);
@@ -23,3 +36,24 @@ for (const radio of userChoices) {
     radio.previousElementSibling.src = randomPokemon["url_image"];
     radio.nextElementSibling.textContent = `${randomPokemon["pokemon"]}`
 };
+
+nextButton.addEventListener('click', () => {
+    console.log('next pressed');
+
+    randomIndices = getDistinctRandomNumbers(pokemonArray.length, 3);
+
+    for (const radio of userChoices) {
+        radio.disabled = false;
+        radio.checked = false;
+        radio.nextElementSibling.style.opacity = 1;
+        radio.parentElement.style.backgroundColor = 'lightgray';
+
+        const radioIndex = userChoices.indexOf(radio);
+        const randomPokemon = pokemonArray[randomIndices[radioIndex]];
+
+        radio.previousElementSibling.src = randomPokemon["url_image"];
+        radio.nextElementSibling.textContent = `${randomPokemon["pokemon"]}`
+    }
+
+    nextButton.classList.toggle('hidden');
+})
